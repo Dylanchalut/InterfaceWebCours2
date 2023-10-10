aa0be71d577218786d284c0f9ac65031d70f7b52
dylan <1852719@cegepoutaouais.qc.ca>
2023-10-09 21:05:52 -0400

    commit

---
+++ ExerciseJS2/script.js
@@ -0,0 +1,17 @@
+let liste = document.getElementById("liste")
+let totals = document.getElementById("totals")
+let total = 81
+function Ajouter()
+{
+    let desc = document.getElementById("desc").value
+    let values = document.getElementById("valeur").value
+    let p = document.createElement("p")
+    p.setAttribute("style", "border-top: black solid 1px")
+    let v = document.createElement("span")
+    p.innerText = desc
+    v.innerText = values + "$"
+    liste.appendChild(p).appendChild(v)
+    total += parseFloat(values);
+    totals.textContent = 'Total : ' + total + '$';
+
+}
