4a48682a785e10f8e317f5de05f30199f33bbc97
info1 <1852719@cegepoutaouais.qc.ca>
2023-12-05 09:55:22 -0500

    commit

--- DemoMockAPI-Json-Ajax/script.js
+++ DemoMockAPI-Json-Ajax/script.js
@@ -12,3 +12,50 @@ function creerCarte(chat) {
             </div>
         </li>`);
 }
+
+//Façon jQuery
+function afficherTout(){
+    $('#aventuriers').text("");
+    $.getJSON('https://656f2fab6529ec1c62378219.mockapi.io/chat')
+        .done(function(chat){
+            chat.forEach(function (chat){
+                creerCarte(chat);
+            });
+        })
+        .fail(function (error){
+            $('.alert').text(error.status).removeClass('d-none');
+        });
+}
+
+afficherTout();
+
+
+function afficherDetails(id){
+    $('#aventuriers').text("");
+    $.getJSON('https://656f2fab6529ec1c62378219.mockapi.io/chat/'+id)
+        .done(function(chat){
+            creerCarte(chat);
+        })
+        .fail(function (error){
+            $('.alert').text(error.status).removeClass('d-none');
+        });
+}
+
+
+function supprimer(id){
+    fetch('https://656f2fab6529ec1c62378219.mockapi.io/chat/'+id, {
+        method: 'DELETE',
+    }).then(res => {
+        if (res.ok) {
+            return res.json();
+        }
+        // handle error
+        throw new Error ("Erreur"+res.status);
+    }).then(chat => {
+        // Do something with deleted task
+        afficherTout()
+    }).catch(error => {
+        // handle error
+        $('.alert').text(error.status).removeClass('d-none');
+    })
+}
